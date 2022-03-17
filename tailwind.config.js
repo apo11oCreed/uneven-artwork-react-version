2a52cc844131b4bca3fc3c4c61ba1c1a0af1b06d
nc <nelson.ncdesigns@gmail.com>
2022-03-17 01:12:49 -0400

    Fixes to gh-pages branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,16 @@ module.exports = {
       'gray-dark': '#273444',
       'gray': '#8492a6',
       'gray-light': '#d3dce6',
+      'splash-hex': '#6BD1D2',
+      'splash-rgba': 'rgba(107, 209, 210,1.0)',
+      'splash-hsla': 'hsla(181, 53%, 62%, 1.0)',
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
+    },
+    backgroundImages:{
+      'default':'url("./public/10475996-3x2-940x627.jpeg")'
     },
     extend: {
       spacing: {
@@ -31,5 +37,6 @@ module.exports = {
       }
     }
   },
+  prefix: 'tw-',
   plugins: [],
 }
