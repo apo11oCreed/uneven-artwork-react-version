39028e2c957047076479060e06eefc2253bc38e6
Nelson Correia <nelson.ncdesigns@gmail.com>
2023-01-08 00:07:19 +0000

    Add modal slice to store

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import portfolioReducer from './slices/portfolioSlice';
 import modalReducer from './slices/modalSlice';
 
+const reducer = {
+  portfolio: portfolioReducer,
+  modal: modalReducer,
+}
+
 export const store = configureStore({
-    reducer: {
-        portfolio: portfolioReducer,
-        modal: modalReducer,
-    },
+    reducer,
 })
